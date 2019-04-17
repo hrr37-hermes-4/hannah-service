7af2ff24cc4cda098f348624dc4572fc74123823
Brian Kwon <bkwon94@gmail.com>
2019-04-17 18:16:53 -0400

    Add second migration file which creates reviews table in postgresql

--- db/migrations/20190417174240_initReviews.js
+++ db/migrations/20190417174240_initReviews.js
@@ -1,11 +1,25 @@
 
-exports.up = function(knex, Promise) {
+exports.up = async (knex, Promise) => {
   await knex.schema.createTable('reviews', (table) => {
     table
-      
+      .increments('id')
+      .primary()
+      .notNullable();
+    table
+      .integer('user_id').notNullable();
+    table
+      .integer('book_id').notNullable();
+    table
+      .string('date').notNullable();
+    table
+      .string('review').notNullable();
+    table
+      .integer('rating').notNullable();
+    table
+      .integer('likes').defaultTo(0);
   });
 };
 
-exports.down = function(knex, Promise) {
-
+exports.down = async (knex, Promise) => {
+  await knex.schema.dropTable('users');
 };
