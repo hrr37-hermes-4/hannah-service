37ff83365058a926d528366258c705d3b90a9c60
Brian Kwon <bkwon94@gmail.com>
2019-04-18 14:01:04 -0400

    Adjust seed function to allow seeding of 10000 entries for now

--- db/seeds/addUsers.js
+++ db/seeds/addUsers.js
@@ -1,10 +1,7 @@
 const generate = require('../generate.js');
 
 exports.seed = async (knex, Promise) => {
-  // while (count <= 10000000) {
-
-  // }
-  // const userData = generate.generateManyUsers();
-  // console.log('users generated', userData.length);
-  // await knex.batchInsert('users', userData, 10000);
+  const userData = generate.generateManyUsers();
+  console.log('users generated', userData.length);
+  await knex.batchInsert('users', userData.slice(0, 10000), 10000);
 };
