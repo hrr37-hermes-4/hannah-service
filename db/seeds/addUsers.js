d3fcd743228c53a59b6d4324409c22bbe7a435bc
Brian Kwon <bkwon94@gmail.com>
2019-04-19 00:07:23 -0400

    Add .then() after truncate to ensure that process finishes before moving on

--- db/seeds/addUsers.js
+++ db/seeds/addUsers.js
@@ -1,8 +1,10 @@
 const generate = require('../generate.js');
 
 exports.seed = async (knex) => {
+  console.time('timing users seeding');
   const userData = generate.generateManyUsers();
   console.log('users generated', userData.length);
   await knex('users').truncate();
   await knex.batchInsert('users', userData);
+  console.timeEnd('timing users seeding');
 };
