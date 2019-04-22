d36d195314c0890843c25985cedf93db637b5256
Brian Kwon <bkwon94@gmail.com>
2019-04-22 14:46:43 -0400

    Add new modles dir for cassandra db folder

--- db/postgreSQL/seeds/addReviews.js
+++ db/postgreSQL/seeds/addReviews.js
@@ -40,36 +40,4 @@ exports.seed = async (knex) => {
 
 
 
-/* seed without csv
-
-console.time('timing reviews seeding');
-  // declare count
-  let count = 0;
-  const incrementCount = () => { count += 1; };
-  // empty table if filled from previous seed
-  await knex('reviews').truncate()
-    .then(async () => {
-      console.log('starting seed');
-      // generate reviews one at a time instead of bulk generation
-      while (count <= 2000) {
-        const fakeReviews = [];
-        // iterate up to desiredAmount each time
-        for (let i = 0; i < 10000; i++) {
-          fakeReviews.push(generate.createFakeReview());
-        }
-        // increment count via function to utilize await
-        await incrementCount();
-        await knex('reviews').insert(fakeReviews);
-      }
-
-      console.timeEnd('timing reviews seeding');
-    });
-
-
-
-
-*/
-
-
-
 
