eb6cb0c86f9d765acdd84c4c7a051b8ea99519d0
Brian Kwon <bkwon94@gmail.com>
2019-04-23 17:58:13 -0400

    Change file structure and refactor reviews csv generator while also creating external file to deal with call count

--- db/cassandra/csv-generators/reviews.js
+++ db/cassandra/csv-generators/reviews.js
@@ -3,25 +3,32 @@ const csvWriter = require('csv-write-stream');
 const writer = csvWriter();
 const fs = require('fs');
 
-const generateReviewsCSV = () => {
+const deleteCSV = () => {
+  fs.unlink(__dirname + '/csv/reviews.csv', err => {
+    if (err) {console.log(err)} else {console.log('Deleted')};
+  })
+};
+
+const generateReviewsCSV = async() => {
   console.log('Starting review csv generation');
   console.time('timing');
   let count = 0;
-  writer.pipe(fs.createWriteStream(__dirname + '/reviews.csv'));
-  for (let i = 0; i < 10000000; i++) {
+  callCount !== 0 ? deleteCSV() : null;
 
-      let review = generator.createFakeReview();
-      writer.write({
-        id: count++,
-        book_id: review.book_id,
-        user_id: review.user_id,
-        date: review.date,
-        review: review.review,
-        rating: review.rating
-      })
-
+  await writer.pipe(fs.createWriteStream(__dirname + `/csv/reviews.csv`));
+  for (let i = startValue; i < startValue + 10000000; i++) {
+    let review = generator.createFakeReview();
+    writer.write({
+      id: count++,
+      book_id: review.book_id,
+      user_id: review.user_id,
+      date: review.date,
+      review: review.review,
+      rating: review.rating
+    })
   }
 
+  callCount++;
   writer.end();
   console.timeEnd('timing');
 }
