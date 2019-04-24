a210605bb1f5518d049976a6982acaffcaf31551
Brian Kwon <bkwon94@gmail.com>
2019-04-24 17:25:51 -0400

    Add two helper functions to get reviews using pg as database

--- db/postgreSQL/index.js
+++ db/postgreSQL/index.js
@@ -2,9 +2,39 @@ const config = require('../../knexfile.js');
 const env = process.env.NODE_ENV || 'development';
 // CONNECT TO POSTGRESQL DB
 const knex = require('knex')(config[env]);
+
 /* ***** POSTGRESQL HELPER FUNCTIONS ***** */
+const getReviews = async(id) => {
+  try {
+    await knex.select()
+      .from('reviews')
+      .where({ book_id: id })
+      .then(reviews =>{
+        console.log(reviews);
+        return reviews;
+      });
+  }
+  catch(err) {
+    throw err;
+  }
+};
 
-const getReviews = (id) => {
+const getRatedReviews = async(id, rating) => {
+  try {
+    let result = await knex.select()
+      .from('reviews')
+      .where({ book_id: id })
+      .andWhere({ rating: rating });
+    console.log(result);
+    return result;
+  }
+  catch(err) {
+    throw err;
+  }
+};
 
-}
 
+
+/* TESTING RESULTS IN CONSOLE */
+// getReviews(439);
+// getRatedReviews(94, 3);
