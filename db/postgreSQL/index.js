5bb1e5a976a1b15f6dbd5d35dae6408c30feb42a
Brian Kwon <bkwon94@gmail.com>
2019-04-24 19:03:37 -0400

    Add remaining helper functions for server-db communication

--- db/postgreSQL/index.js
+++ db/postgreSQL/index.js
@@ -4,15 +4,14 @@ const env = process.env.NODE_ENV || 'development';
 const knex = require('knex')(config[env]);
 
 /* ***** POSTGRESQL HELPER FUNCTIONS ***** */
+
 const getReviews = async(id) => {
   try {
-    await knex.select()
+    let reviews = await knex.select()
       .from('reviews')
-      .where({ book_id: id })
-      .then(reviews =>{
-        console.log(reviews);
-        return reviews;
-      });
+      .where({ book_id: id });
+    console.log(reviews);
+    return reviews
   }
   catch(err) {
     throw err;
@@ -21,20 +20,80 @@ const getReviews = async(id) => {
 
 const getRatedReviews = async(id, rating) => {
   try {
-    let result = await knex.select()
+    let ratedReviews = await knex.select()
       .from('reviews')
       .where({ book_id: id })
       .andWhere({ rating: rating });
-    console.log(result);
-    return result;
+    console.log(ratedReviews);
+    return ratedReviews;
   }
   catch(err) {
     throw err;
   }
 };
 
+const getUser = async(userId) => {
+  try {
+    let user = await knex.select('username')
+      .from('users')
+      .where({ user_id: userId });
+    console.log(user);
+    return user;
+  }
+  catch(err) {
+    throw err;
+  }
+};
 
+const getAllUsers = async() => {
+  try {
+    console.time('get all users time');
+    let users = await knex.select().from('users');
+    await console.timeEnd('get all users time');
+    // await console.log(users);
+    return users;
+  }
+  catch(err) {
+    throw err;
+  }
+};
+
+const postReview = async(review, rating, bookId, userId) => {
+  try {
+    let date = new Date();
+    date = date.toString();
+    date = date.slice(4, 10) + ', ' + date.slice(11, 15);
+    await knex('reviews')
+      .insert({
+        review: review,
+        rating: rating,
+        book_id: bookId,
+        user_id: userId,
+        date: date
+      });
+      await console.log('Posted');
+  }
+  catch(err) {
+    throw err;
+  }
+};
+
+const addLike = async(reviewId) => {
+  try {
+    await knex('reviews')
+      .where('id', '=', reviewId)
+      .increment('likes', 1)
+    await console.log('Updated likes');
+  }
+  catch(err) {
+    throw err;
+  }
+};
 
 /* TESTING RESULTS IN CONSOLE */
 // getReviews(439);
 // getRatedReviews(94, 3);
+// getUser(9000);
+// getAllUsers();
+// postReview('Hello this is my review of this awesome book', 3, 2, 33);
+// addLike(5);
