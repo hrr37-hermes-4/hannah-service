1c8c744fde4973a0ba85e254a7602466934c4cca
Brian Kwon <bkwon94@gmail.com>
2019-04-29 18:32:57 -0400

    Final new relic config/setup for local testing and ec2 db seeding files

--- server/app.js
+++ server/app.js
@@ -20,6 +20,7 @@ app.get('/books/:id/reviews', async (req, res) => {
   const { id } = req.params;
   try {
     const reviews = await pg.getReviews(id);
+    console.log(reviews);
     res.json(reviews);
   } catch (err) {
     res.json(err);
